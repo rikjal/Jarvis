79960bec0dded04fd37bff5cc3c2be40dd74ca97
rikjal <saptarshi.jpg@gmail.com>
2020-06-23 01:55:08 +0530

    First

--- jarvis.py
+++ jarvis.py
@@ -5,6 +5,7 @@ import wikipedia as wk
 import webbrowser as wb
 import os
 
+# Initialization
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
